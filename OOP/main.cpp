91d2364d21d941177bded3b189c4dbc538a92e32
River Fisher <goldenarius@gmail.com>
2016-12-09 08:11:31 +1000

    Update main.cpp

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
-#include <string>
 #include "Check.h"
+
+using namespace std;
 
 int main() {
     setlocale(LC_ALL, "rus");
 
-    string products = {""};
+    const char *products[6] = {
+        "Навага свежемороженая (брикет)", "Тефтели из говядины", "Пельмени \"Сибирские\"", "Блины (ветчина и сыр)",
+        "Фрукты замороженные", "Хинкали \"Сочинские\""
+    };
+    int prices[6] = {270, 135, 260, 192, 380, 410};
+    double weights[6] = {1, 0.5, 1, 0.135, 0.6, 0.8};
+    int sum[6] = {3, 4, 1, 6, 1, 8};
 
-    Check product;
+    Check product[6];
 
-    product.setName("Навага свежемороженая (брикет)");
-    product.setPrice(270);
-    product.setWeight(1);
+    for (int i = 0; i < 6; i++) {
+        product[i].setName(products[i]);
+        product[i].setPrice(prices[i]);
+        product[i].setWeight(weights[i]);
 
-    product.setSum(16);
+        product[i].setSum(sum[i]);
 
-    product.printProductInfo();
-    product.printBuyInfo();
+        product[i].printProductInfo();
+        product[i].printBuyInfo();
 
-    (&product)->~Check();
+        (&product[i])->~Check();
+    }
 
     system("pause");
     return 0;
